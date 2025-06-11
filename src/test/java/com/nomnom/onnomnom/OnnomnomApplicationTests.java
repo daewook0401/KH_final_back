7714398dc9593eacf32ce38fb9a677e1b718e3e2
daewook0401 <daewook0401@naver.com>
2025-06-11 11:30:57 +0900

    base

---
+++ src/test/java/com/nomnom/onnomnom/OnnomnomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nomnom.onnomnom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnnomnomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
