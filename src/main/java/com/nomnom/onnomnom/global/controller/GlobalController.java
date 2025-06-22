282bee97ba5c0c5dff73d903e50d84928e14ab24
daewook0401 <daewook0401@naver.com>
2025-06-22 17:31:19 +0900

    globalController 추가

--- src/main/java/com/nomnom/onnomnom/global/controller/GlobalController.java
+++ src/main/java/com/nomnom/onnomnom/global/controller/GlobalController.java
@@ -1,5 +1,19 @@
 package com.nomnom.onnomnom.global.controller;
 
-public class GlobalController {
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class GlobalController {
+    
+    @GetMapping
+    public ResponseEntity<String> global(){
+        return ResponseEntity.ok().body("성공");
+    }
 }
