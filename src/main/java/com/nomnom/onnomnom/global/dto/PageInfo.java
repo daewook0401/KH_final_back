08d2b714153c20afcab9efdeb79c2062385ad66c
jiwoolee2 <dlwldn1009@naver.com>
2025-06-23 09:30:50 +0900

    pagination

---
+++ src/main/java/com/nomnom/onnomnom/global/dto/PageInfo.java
@@ -0,0 +1,16 @@
+package com.nomnom.onnomnom.global.dto;
+
+import lombok.Builder;
+
+@Builder
+public class PageInfo {
+
+	private int currentPage;    // 현재 페이지 번호
+	private int totalPages;     // 전체 페이지 수
+	private int pageSize;       // 한 화면에 보여질 페이지 수 (추가로 필요할 경우)
+	private int startPage;      // 시작 페이지 번호
+	private int endPage;        // 끝 페이지 번호
+	private int boardNoPerPage;   // 한 페이지에 보여질 게시글 수
+	private int totalBoardNo;     // 전체 게시글 수
+}
+
