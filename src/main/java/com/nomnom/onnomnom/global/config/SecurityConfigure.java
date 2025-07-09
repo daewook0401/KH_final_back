5f85f1f603dbcab8e90ca570dfb9f3ad7457f73c
KimDaeWook <62898841+daewook0401@users.noreply.github.com>
2025-07-09 10:21:21 +0900

    Update SecurityConfigure.java

--- src/main/java/com/nomnom/onnomnom/global/config/SecurityConfigure.java
+++ src/main/java/com/nomnom/onnomnom/global/config/SecurityConfigure.java
@@ -59,8 +59,7 @@ public class SecurityConfigure {
     public CorsConfigurationSource corsConfigurationSource(){
 		CorsConfiguration configuration = new CorsConfiguration();
 		// configuration.setAllowedOrigins(Arrays.asList("192.168.219.**:**"));
-		configuration.setAllowedOrigins(Arrays.asList("https://onnomnom.shop"));
-		//configuration.setAllowedOriginPatterns(Arrays.asList("https://onnomnom.shop"));
+		configuration.setAllowedOriginPatterns(Arrays.asList("https://onnomnom.shop"));
 		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
 		configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
 		configuration.setAllowCredentials(true);
